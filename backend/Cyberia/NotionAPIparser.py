c6a82749071de580c789606a8fbe971beddab5f0
siyuancheng178 <siyuanc6794@gmail.com>
2022-01-30 22:40:33 +0800

    Cyberia

---
+++ backend/Cyberia/NotionAPIparser.py
@@ -0,0 +1,64 @@
+import json
+
+def parse(data):
+    if (data["object"] == "list"):
+        print("debug")
+        return parseList(data["results"])
+    
+
+def parseList(data):
+    print(data)
+    list = []
+    for i in data:
+        if (i["type"] == "heading_1"):
+            list.append(parseHeading(i))
+        elif (i["type"] == "paragraph"):
+            list.append(parseParagraph(i))
+        elif (i["type"] == "bulleted_list_item"):
+            list.append(parseBulletList(i))
+    return list
+
+
+def parseHeading(data):
+    result = dict()
+    result["type"] = "heading"
+    result["content"] = data["heading_1"]["text"][0]["plain_text"]
+    return result
+
+def parseParagraph(data):
+    list = []
+    for i in data["paragraph"]["text"]:
+        list.append(parseText(i))
+    
+    result = dict()
+    result["type"] = "paragraph"
+    result["content"] = list
+    return result
+
+def parseText(data):
+    result = dict()
+    result["type"] = "text"
+    result["content"] = data["plain_text"]
+    
+    special_attribute = dict()
+    for attribute in data["annotations"]:
+        if ((data["annotations"][attribute] == True) or 
+        (attribute == "color" and data["annotations"][attribute] != "default")):
+            special_attribute[attribute] = True
+    
+    result["attribute"] = special_attribute
+    return result
+
+def parseBulletList(data):
+    result = dict()
+    print(data)
+    result["type"] = "bulleted_list_item"
+    result["content"] = data["plain_text"]
+    special_attribute = dict()
+    for attribute in data["annotations"]:
+        if ((data["annotations"][attribute] == True) or 
+        (attribute == "color" and data["annotations"][attribute] != "default")):
+            special_attribute[attribute] = True
+    
+    result["attribute"] = special_attribute
+    return result
