817085dbe1be5346b21bb96a1680ba1d8fa473cd
siyuancheng178 <siyuanc6794@gmail.com>
2022-02-08 19:55:15 +0800

    refactor code

--- backend/Cyberia/views.py
+++ backend/Cyberia/views.py
@@ -1,6 +1,5 @@
 from urllib import response
 from rest_framework.decorators import api_view
-from django.http import JsonResponse
 from dotenv import load_dotenv
 from rest_framework.response import Response
 from rest_framework import status
@@ -15,6 +14,10 @@ load_dotenv(dotenv_path)
 token = os.getenv('token')
 version = os.getenv('version')
 components = dict()
+
+NOTION_PAGE_URL = 'https://api.notion.com/v1/blocks/{blockid}/children'
+NOTION_HEADER = {'Notion-Version': version, 'Authorization': token}
+
 
 # Create your views here.
 
@@ -46,54 +49,54 @@ def cyberia(request):
 
 
 def overview():
-    url = 'https://api.notion.com/v1/blocks/40712ecd5d6246aab46d9d51442c1727/children'
-    headers = {'Notion-Version': version, 'Authorization': token}
-    response = requests.get(url, headers=headers)
+    blockid = '40712ecd5d6246aab46d9d51442c1727'
+    url = NOTION_PAGE_URL.format(blockid = blockid)
+    response = requests.get(url, headers=NOTION_HEADER)
     data = response.json()
     components["overview"] = parse(data)
 
 
 def benefit():
-    url = 'https://api.notion.com/v1/blocks/1b95121ff4e24ab38c155c99c42675d9/children'
-    headers = {'Notion-Version': version, 'Authorization': token}
-    response = requests.get(url, headers=headers)
+    blockid = '1b95121ff4e24ab38c155c99c42675d9'
+    url = NOTION_PAGE_URL.format(blockid = blockid)
+    response = requests.get(url, headers=NOTION_HEADER)
     data = response.json()
     components["benefits"] = parse(data)
 
 
 def time():
-    url = 'https://api.notion.com/v1/blocks/95e9bd5dd0394a2bb8e6b5c10b3433b3/children'
-    headers = {'Notion-Version': version, 'Authorization': token}
-    response = requests.get(url, headers=headers)
+    blockid = '95e9bd5dd0394a2bb8e6b5c10b3433b3'
+    url = NOTION_PAGE_URL.format(blockid = blockid)
+    response = requests.get(url, headers=NOTION_HEADER)
     data = response.json()
     components["time"] = parse(data)
 
 
 def registriation():
-    url = 'https://api.notion.com/v1/blocks/8bfb13152625483f996f26c2e7d2b371/children'
-    headers = {'Notion-Version': version, 'Authorization': token}
-    response = requests.get(url, headers=headers)
+    blockid = '8bfb13152625483f996f26c2e7d2b371'
+    url = NOTION_PAGE_URL.format(blockid = blockid)
+    response = requests.get(url, headers=NOTION_HEADER)
     data = response.json()
     components["registriation"] = parse(data)
 
 def acadia_training():
-    url = 'https://api.notion.com/v1/blocks/d36eabdea553461dadf34410534deb25/children'
-    headers = {'Notion-Version': version, 'Authorization': token}
-    response = requests.get(url, headers=headers)
+    blockid = 'd36eabdea553461dadf34410534deb25'
+    url = NOTION_PAGE_URL.format(blockid = blockid)
+    response = requests.get(url, headers=NOTION_HEADER)
     data = response.json()
     components["acadia_training"] = parse(data)
 
 def trainers():
-    url = 'https://api.notion.com/v1/blocks/949adb5dff604986be8785c4d65d7c70/children'
-    headers = {'Notion-Version': version, 'Authorization': token}
-    response = requests.get(url, headers=headers)
+    blockid = '949adb5dff604986be8785c4d65d7c70'
+    url = NOTION_PAGE_URL.format(blockid = blockid)
+    response = requests.get(url, headers=NOTION_HEADER)
     data = response.json()
     components["trainers"] = parse(data)
 
 def contact():
-    url = 'https://api.notion.com/v1/blocks/139602eef0454c4aa124dc5363b272a0/children'
-    headers = {'Notion-Version': version, 'Authorization': token}
-    response = requests.get(url, headers=headers)
+    blockid = '139602eef0454c4aa124dc5363b272a0'
+    url = NOTION_PAGE_URL.format(blockid = blockid)
+    response = requests.get(url, headers=NOTION_HEADER)
     data = response.json()
     components["contact"] = parse(data)
 
