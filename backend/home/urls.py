6a195defeb104567c16b6cbb240c58a564e8412f
DystoriaX <dascogabriel@gmail.com>
2023-01-02 22:11:04 +0800

    home: fix styling

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-  path('training-workshops/', views.trainingworkshops),
-  path('elevate-the-creativity/', views.elevatecreativity),
-  path('study-print-scan/', views.studyprintscan),
+  path('training-workshops/', views.training_workshops),
+  path('elevate-the-creativity/', views.elevate_creativity),
+  path('study-print-scan/', views.study_print_scan),
 ]
--- backend/home/views.py
+++ backend/home/views.py
@@ -19,21 +19,21 @@ NOTION_HEADER = {'Notion-Version': version, 'Authorization': token}
 
 # Create your views here.
 @api_view(['Get'])
-def trainingworkshops(request):
+def training_workshops(request):
     TRAINING_WORKSHOPS_URL = 'c5cdbf3484cc4f77821e0691b8b4a47b'
     data = [get_parsed_data(TRAINING_WORKSHOPS_URL)]
     return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(['Get'])
-def elevatecreativity(request):
+def elevate_creativity(request):
     ELEVATE_CREATIVITY_URL = 'aab5e676774444188471c7b7a105c52c'
     data = [get_parsed_data(ELEVATE_CREATIVITY_URL)]
     return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(['Get'])
-def studyprintscan(request):
+def study_print_scan(request):
     STUDY_PRINT_SCAN_URL = 'efea5f3b0a8d4af4879798c6a347f3bc'
     data = [get_parsed_data(STUDY_PRINT_SCAN_URL)]
     return Response(data, status=status.HTTP_200_OK)
