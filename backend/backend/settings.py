074a3b6021c509628b154e32e4993f747dd4709d
vjo-anne209 <77625659+vjo-anne209@users.noreply.github.com>
2023-02-19 12:55:02 +0800

    Update allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 #production url: 'nussucommit-backend.herokuapp.com'
 
